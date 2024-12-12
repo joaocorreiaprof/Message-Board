742dbb1895c158a65b5e2b10e030156963f56904
João Correia <joaocorreiaprof@gmail.com>
2024-12-12 11:41:25 +0000

    Styled homepage and form

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const express = require("express");
 const path = require("node:path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 const messages = [
   {
-    text: "Hi there!",
+    text: "Believe you can and you're halfway there.",
     user: "Amando",
     added: new Date(),
   },
   {
-    text: "Hello World!",
+    text: "The only way to do great work is to love what you do.",
     user: "Charles",
+    added: new Date(),
+  },
+
+  {
+    text: "You are never too old to set another goal or to dream a new dream.",
+    user: "Eleanor",
+    added: new Date(),
+  },
+  {
+    text: "The best time to plant a tree was 20 years ago. The second best time is now.",
+    user: "Liam",
+    added: new Date(),
+  },
+  {
+    text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    user: "Harper",
     added: new Date(),
   },
 ];
@@ -18,8 +36,8 @@ const messages = [
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", { messages: messages });
